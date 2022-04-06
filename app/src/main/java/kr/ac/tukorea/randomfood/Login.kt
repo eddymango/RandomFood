b61980bcbb0015ebf98f833b347e61403c9b33f9
eddymango <eddymango@naver.com>
2022-04-06 22:09:44 +0900

    시작화면- 화면 터치시 룰렛 화면으로 이동

--- app/src/main/java/kr/ac/tukorea/randomfood/Login.kt
+++ app/src/main/java/kr/ac/tukorea/randomfood/Login.kt
@@ -8,17 +8,20 @@ import kr.ac.tukorea.randomfood.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
 
-    val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+    val binding by lazy { ActivityLoginBinding.inflate(layoutInflater)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
 
         binding.imageView.setOnClickListener {
             Toast.makeText(this,"이미지 클릭",Toast.LENGTH_SHORT).show()
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
+        }
 
+        binding.btn.setOnClickListener {
+            Toast.makeText(this,"클릭 리스너 됩니다,",Toast.LENGTH_SHORT).show()
         }
 
 
